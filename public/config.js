52d59d87b38bfa4ec61dac5b29779086ac2fa668
reinjang <rjwslagter@gmail.com>
2025-07-10 21:55:18 +0200

    Set API_URL to /api in config.js for nginx proxy

--- public/config.js
+++ public/config.js
@@ -1,6 +1,6 @@
 // Runtime configuration for Finance Planner
 // This file can be updated without rebuilding the application
 window.FINANCE_CONFIG = {
-  API_URL: 'http://92.62.118.113:8000',
+  API_URL: '/api',
   APP_TITLE: 'Finance Planner'
 }; 
